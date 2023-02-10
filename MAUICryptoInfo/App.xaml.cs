8a8354ae890bcc4b66457496bae62b9e9cb0d4f6
Dat Doan <42245488+SmileDragonfly@users.noreply.github.com>
2023-02-10 09:32:57 +0700

    Add project MAUICryptoInfo

---
+++ MAUICryptoInfo/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MAUICryptoInfo;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MAUICryptoInfo/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MAUICryptoInfo;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
